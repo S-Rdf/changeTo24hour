7cfcf4e101a87fd364f80d0c9d578443e00b5399
samiradfar <samiradfar@yahoo.com>
2017-02-12 12:53:33 +0330

    برنامه عددی بین 1 تا 12 بعنوان ورودی میگیرد و در صورت فالس بودن همان را نمایش داده و در صورت ترو بودن 12 واحد به آن اضافه میکند. اگر عدد خارج از رنج باشد یا حروف وارد شود پیام خطا میدهد

---
+++ changeTo24hour/changeTo24hour/frmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace changeTo24hour
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChange = new System.Windows.Forms.Button();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(103, 105);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 0;
+            this.btnChange.Text = "تبدیل";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // txtHour
+            // 
+            this.txtHour.Location = new System.Drawing.Point(103, 66);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(75, 20);
+            this.txtHour.TabIndex = 2;
+            this.txtHour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+           // this.txtHour.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtHour);
+            this.Controls.Add(this.btnChange);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox txtHour;
+    }
+}
+
---
+++ changeTo24hour/changeTo24hour/frmMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace changeTo24hour
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            bool isPM=true;
+
+            if (CheckRenj(txtHour.Text) == true)
+                ChangeHour(int.Parse(txtHour.Text),isPM);
+        }
+
+        public bool CheckRenj(string txtHour)
+        {
+            int hour;
+            bool isSuccess = int.TryParse(txtHour,out hour);
+
+            if (isSuccess == true)
+                if (hour >= 1 && hour <= 12)
+                    return true;
+                else
+                {
+                    MessageBox.Show("عدد ساعت را بین 1 تا 12 وارد کنید");
+                    return false;
+                }                   
+            else
+            {
+                MessageBox.Show("عدد ساعت را بین 1 تا 12 وارد کنید");
+                return false;
+            }     
+        }
+
+        void ChangeHour(int hour,bool isPM)
+        {
+            if (isPM == false)
+                MessageBox.Show(hour.ToString());
+            else
+                MessageBox.Show((hour + 12).ToString());
+        }
+    }
+}
